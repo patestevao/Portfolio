e8fa839b3a98ea5336e50de24eb815d8b93f41b7
pat-estevao <pat.estevao@live.com>
2015-07-01 21:39:41 -0300

    links all projects pages

--- app.py
+++ app.py
@@ -6,5 +6,46 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/index.html')
+def index_return():
+	return render_template('index.html')
+
+@app.route('/dispersion.html')
+def dispersion():
+	return render_template('dispersion.html')
+
+@app.route('/double_exposure.html')
+def double_exposure():
+	return render_template('double_exposure.html')
+
+@app.route('/drawing.html')
+def drawing():
+	return render_template('drawing.html')
+
+@app.route('/halftone.html')
+def halftone():
+	return render_template('halftone.html')
+
+@app.route('/lego.html')
+def lego():
+	return render_template('lego.html')
+
+@app.route('/mosaic.html')
+def mosaic():
+	return render_template('mosaic.html')
+
+@app.route('/stereoscope.html')
+def stereoscope():
+	return render_template('stereoscope.html')
+
+@app.route('/text_portrait.html')
+def text_portrait():
+	return render_template('text_portrait.html')
+
+@app.route('/warhol.html')
+def warhol():
+	return render_template('warhol.html')
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
